c5265f69dac42db40fed51aacf6bb0d32c0fed4b
sanjeev06kmr <sanjeev06kmr@gmail.com>
2023-12-14 08:21:07 -0700

    NPM Init

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const reactHeader = React.createElement(
   { id: "headerId" },
   "Hello World - from App.js  React!!"
 );
+
+const content = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am Header 1"),
+    React.createElement("h1", {}, "I am Header 2"),
+  ])
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(reactHeader);
+root.render(content);
