2a72142b4ca9cd554e6abb91a6307ffaedd48236
sanjeev06kmr <sanjeev06kmr@gmail.com>
2023-12-14 07:15:17 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const reactHeader = React.createElement(
+  "h1",
+  { id: "headerId" },
+  "Hello World - from App.js  React!!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(reactHeader);
